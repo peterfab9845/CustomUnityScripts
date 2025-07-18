9450dc975f1242f56e56aefac0337163dd32637f
Peter Fabinski <git@peterfab.com>
2025-07-18 06:14:30 -0400

    Add scripts from testing material bug

---
+++ CustomUnityScripts/Editor/UnloadUnusedAssets.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+public class UnloadUnusedAssets {
+
+    [MenuItem("Tools/Unload Unused Assets", false, 170)]
+    private static void DoUnloadUnusedAssets()
+    {
+        // https://docs.unity3d.com/2022.3/Documentation/ScriptReference/EditorUtility.UnloadUnusedAssetsImmediate.html
+        // Unloads assets that are not used.
+        // An asset is deemed to be unused if it isn't reached after walking the whole game object hierarchy, including script components. Static variables are also examined.
+        // This method differs from Resources.UnloadUnusedAssets in that it will wait for asset garbage collection to finish before returning.
+        EditorUtility.UnloadUnusedAssetsImmediate();
+    }
+}
---
+++ misc/TestMaterialBug.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class TestMaterialBug {
+
+    // setup:
+    // matA = blank material
+    // matB = variant of matA, also blank
+    // matC = variant of matB, tex set
+    // matD = variant of matB, tex set
+
+    // step 1: flatten the material variants, they keep the tex. Then unload unused assets
+    [MenuItem("Tools/Test Material Bug part1", false, 100)]
+    private static void DoTestMaterialBug1()
+    {
+        Material matC = (Material) AssetDatabase.LoadAssetAtPath("Assets/test/matC.mat", typeof(Material));
+        Material matD = (Material) AssetDatabase.LoadAssetAtPath("Assets/test/matD.mat", typeof(Material));
+        matC.parent = null;
+        matD.parent = null;
+        EditorUtility.UnloadUnusedAssetsImmediate();
+    }
+
+    // step 2: reparent matC. It loses all its properties.
+    [MenuItem("Tools/Test Material Bug part2", false, 101)]
+    private static void DoTestMaterialBug2()
+    {
+        Material matB = (Material) AssetDatabase.LoadAssetAtPath("Assets/test/matB.mat", typeof(Material));
+        Debug.Log(matB.parent);
+        Material matC = (Material) AssetDatabase.LoadAssetAtPath("Assets/test/matC.mat", typeof(Material));
+        matC.parent = matB;
+    }
+
+    // step 3: reparent matD. It does NOT lose all its properties (and neither do any following ones).
+    [MenuItem("Tools/Test Material Bug part3", false, 102)]
+    private static void DoTestMaterialBug3()
+    {
+        Material matB = (Material) AssetDatabase.LoadAssetAtPath("Assets/test/matB.mat", typeof(Material));
+        Debug.Log(matB.parent);
+        Material matD = (Material) AssetDatabase.LoadAssetAtPath("Assets/test/matD.mat", typeof(Material));
+        matD.parent = matB;
+    }
+}
